8d9ce65b93aa7b9d656a4e7e77eabc41e9e108c4
MirnaMilad <mirnamilad0101@gmail.com>
2024-11-10 11:33:45 +0200

    BE init

---
+++ Personal-Blog-BE/personal-blogs-app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+import models
+from database import engine
+from fastapi.middleware.cors import CORSMiddleware
+from routers import auth , personalBlogs
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+models.Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router)
+app.include_router(personalBlogs.router)
